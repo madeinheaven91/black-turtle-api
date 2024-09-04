06477a557eb27ff980d60ffbc39a88f2a1986726
madeinheaven91 <exspectanss@gmail.com>
2024-09-04 17:28:51 +0300

    start + fio

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import asyncio
 import logging
 import sys
-import threading
 from os import getenv
 
-from aiogram import Bot, Dispatcher, F, Router, types
+from aiogram import Bot, Dispatcher, F, Router
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
-from aiogram.filters import Command, CommandStart
-# from aiogram.filters.command import Command
+from aiogram.filters import Command
 from aiogram.types import Message
 
 from service import handle_fio, handle_groups, handle_lessons
@@ -32,11 +30,10 @@ async def msg_handler(message: Message) -> None:
         case "группы":
             await handle_groups(message, tokens)
 
-
-@dp.message(Command("help"))
-async def cmd_help(message: Message) -> None:
+@dp.message(Command("start"))
+async def cmd_start(message: Message) -> None:
     await message.answer(
-"""   Привет! Я помогу тебе узнать все о расписаниях, группах и преподавателях
+        """   Привет! Я помогу тебе узнать все о расписаниях, группах и преподавателях
 
 На данный момент можно узнать <b>пары</b> и <b>фио</b>
 
@@ -45,12 +42,34 @@ async def cmd_help(message: Message) -> None:
 Примечание:    "сегодня" писать необязательно, "пары 921" тоже будет работать
 
 <b>Фио:</b>     <i>фио  [фамилия]</i>
-Пример:   фио Димитриев"""
+Пример:   фио Димитриев
+Примечание:   имена взяты с сайта tatar.edu, поэтому информация может быть устаревшей или неправильной
+        
+<i>Если нашли ошибку, пишите сюда: @madeinheaven91</i>"""
+    )
+
+@dp.message(Command("help"))
+async def cmd_help(message: Message) -> None:
+    await message.answer(
+"""<b>Пары:</b>    <i>пары  [номер группы]  [сегодня | завтра | неделя]</i>
+Пример:    пары 921 сегодня
+Примечание:    "сегодня" писать необязательно, "пары 921" тоже будет работать
+
+<b>Фио:</b>     <i>фио  [фамилия]</i>
+Пример:   фио Димитриев
+Примечание:   имена взяты с сайта tatar.edu, поэтому информация может быть устаревшей или неправильной
+        
+<i>Если нашли ошибку, пишите сюда: @madeinheaven91</i>"""
     )
 
 
 async def main() -> None:
     TOKEN = getenv("BOT_TOKEN")
+
+    if TOKEN is None:
+        print("Token not found!")
+        return
+
     bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
     dp.include_router(router)
