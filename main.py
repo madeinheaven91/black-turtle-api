bbb496b42ee7c1ada792d5af877ec0bd8de459e5
madeinheaven91 <exspectanss@gmail.com>
2024-09-09 22:27:39 +0300

    fix breaking change

--- main.py
+++ main.py
@@ -15,14 +15,19 @@ from aiogram.types import Message
 from dotenv import load_dotenv
 
 from data import *
-from src.db import db_commit_close, db_connect
+from src.db import db_commit_close, db_connect, db_init
 from src.dispatcher import *
-from src.handlers import (handle_bell, handle_exception, handle_fio,
-                          handle_groups, handle_help, handle_lessons)
+from src.handlers import (
+    handle_bell,
+    handle_exception,
+    handle_fio,
+    handle_groups,
+    handle_help,
+    handle_lessons,
+)
 from src.keyboards import *
 from src.state_machines import *
 from src.utils import log_request, safe_message
-
 
 ################
 ##### MAIN #####
@@ -31,23 +36,11 @@ from src.utils import log_request, safe_message
 
 async def main() -> None:
     load_dotenv()
-    
+    locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
 
     conn, cur = await db_connect()
-
-    cur.execute(
-        """CREATE TABLE IF NOT EXISTS Chat (
-    id BIGINT PRIMARY KEY,
-    name VARCHAR(255),
-    is_group BOOLEAN,
-    selected_group_id VARCHAR(255) null
-);"""
-    )
-
+    await db_init()
     await db_commit_close(conn, cur)
-
-    locale.setlocale(locale.LC_TIME, "ru_RU.UTF-8")
-    
 
     dp.include_router(router)
     await dp.start_polling(bot)
